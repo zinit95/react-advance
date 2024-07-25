575a9268c2961e09c7f88441de164ead89715bc2
zinit95 <kyj32323@naver.com>
2024-07-25 09:44:57 +0900

    # 2. React.memo로 불필요한 자식컴포넌트 리렌더링 방지

--- src/optimizing/components/Counter/Counter.js
+++ src/optimizing/components/Counter/Counter.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import React, { useState, memo } from 'react';
 
 import IconButton from '../UI/IconButton';
 import MinusIcon from '../UI/Icons/MinusIcon';
@@ -59,4 +60,4 @@ const Counter = ({ initialCount }) => {
     </section>
   );
 };
-export default Counter;
+export default memo(Counter);
